5cbf6a0c8e046bc85c83ae7dc08a1b8c245d72bc
katherine <katherinejc103@gmail.com>
2022-08-25 19:11:00 -0400

    commit tree 43fec0fb744f7f45096fbce129e7f5adb28e41de

--- control_flow.rb
+++ control_flow.rb
@@ -1,9 +1,21 @@
 def admin_login(username, password)
   # your code here
+  # if username == admin || username == admin
+  #   puts 
+
 end
 
 def hows_the_weather(temperature)
   # your code here
+  case weather 
+  when temperature < 30
+    "Its brisk out there!"
+  when 40 < temperature < 65 
+    "It's a little chilly out there!"
+  when 85 < temperature
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
 end
 
 def fizzbuzz(num)
